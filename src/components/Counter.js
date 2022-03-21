0b1522a76471be49388b2cf82d9a665344d26ed9
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-03-20 23:36:04 -0700

    counter component functioning

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,6 +7,10 @@ https://tk-assets.lambdaschool.com/59036a85-0980-42c8-81ad-9afc8354497f_counter-
 How many slices of state do you think are necessary to act as "sources of truth" for all
 the things that change in this widget? Give it some thought before continuing reading!
 
+is blue/red
+is even/odd
+
+
 A naive developer might say 3 different slices:
   - The count
   - Whether the text is color crimson or royalblue
@@ -46,32 +50,38 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Counter() {
   /* STEP 1 */
+  const [count, setCount] = useState(0);
 
   const increment = () => {
     /* STEP 4 */
+     setCount(count + 1);
   };
   const decrement = () => {
     /* STEP 5 */
+    setCount(count-1);
   };
   const reset = () => {
     /* STEP 6 */
+    setCount(0);
   };
+
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: count % 2 === 0 ? "royalblue" : "crimson" , /* STEP 2 */
   };
 
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number 0 is even {/* STEP 3 */}
+        Number {count} is even {count % 2 === 0 ? "even" : "odd"} {/**<-- step 3 */}
+
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
